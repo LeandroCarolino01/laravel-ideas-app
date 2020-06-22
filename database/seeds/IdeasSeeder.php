327b4819220d9c4d61bac573484df445b8c4fc12
Leandro.Carolino <carolino.leandro@gmail.com>
2020-06-22 09:16:35 +0200

    feat(data)populate date in db

--- database/seeds/IdeasSeeder.php
+++ database/seeds/IdeasSeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class IdeasSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        factory(App\Idea::class, 10)->create();
+        //
     }
 }
