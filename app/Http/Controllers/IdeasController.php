8439363118326ab26d7992654885aed4bb5c08be
Leandro.Carolino <carolino.leandro@gmail.com>
2020-06-23 11:16:52 +0200

    feat(btn)add btns

--- app/Http/Controllers/IdeasController.php
+++ app/Http/Controllers/IdeasController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Idea;
+
 class IdeasController extends Controller
 {
     //
     public function index(){
-        return view('ideas.index');
+        // another way to get data from the back end
+        // $ideas = Idea::all();
+
+        return view('ideas.index')->with('ideas', Idea::all());
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Idea::class, 10)->create();
+        factory(App\Idea::class, 5)->create();
     }
 }
