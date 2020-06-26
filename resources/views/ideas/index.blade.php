3476d09fb826d48c23650ba283bbcd267230aa06
Leandro.Carolino <carolino.leandro@gmail.com>
2020-06-26 10:05:29 +0200

    feat(layout)add layout

--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -1,37 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <title>Ideas</title>
-</head>
-<body>
-   <div class="container">
-   <h1 class="text-center my-5">Ideas page</h1>
-   <div class="row justify-content-center">
-    <div class="col-md-8 offet-md-2">
-    <card class="card card-default">
-       <div class="card-header">
-         Ideas
-       </div>
-       <div class="card-body">
-        <ul class="list-group">
-                @foreach($ideas as $idea)
+@extends('layouts.app')
 
-                <li class="list-group-item">
-                    {{ $idea->name }}
-                    <a href="/ideas/{{ $idea->id }}" class="btn btn-primary btn-sm float-right">View</a>
-                </li>
-                @endforeach
-            </ul>
-            <a href="/">Back</a>
-       </div>
-    </card>
+@section('title')
+    Ideas List
+@endsection
+
+@section('content')
+
+<h1 class="text-center my-5">Ideas page</h1>
+    <div class="row justify-content-center">
+        <div class="col-md-8 offet-md-2">
+        <card class="card card-default">
+        <div class="card-header">
+            Ideas
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                    @foreach($ideas as $idea)
+
+                    <li class="list-group-item">
+                        {{ $idea->name }}
+                        <a href="/ideas/{{ $idea->id }}" class="btn btn-primary btn-sm float-right">View</a>
+                    </li>
+                    @endforeach
+                </ul>
+                <a href="/">Back</a>
+        </div>
+        </card>
+        </div>
     </div>
-   </div>
-    
-   
-   </div>
-</body>
-</html>
+ @endsection
--- resources/views/ideas/show.blade.php
+++ resources/views/ideas/show.blade.php
@@ -1,17 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <title>Idea Item</title>
-</head>
-<body>
-    <div class="container">
-        <h1 class="text-center my-5">
+@extends('layouts.app')
+
+@section('title')
+    Single Idea: {{ $idea->name}}
+@endsection
+
+@section('content')
+    <h1 class="text-center my-5">
         {{ $idea->name }}
         </h1>
-       <div class="row justify-content-center">
+        <div class="row justify-content-center">
             <div class="col-md-6">
             <div class="card card-default">
             <div class="card-header">
@@ -20,9 +17,8 @@
             <div class="card-body">
                 {{ $idea->description}}
             </div>
+         <a href="/">back</a>
         </div>
-            </div>
-       </div>
+        </div>
     </div>
-</body>
-</html>
+@endsection
